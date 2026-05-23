64be552e6aacb43bfe67d67060e2b1eb59c44875
Anshu Sharma <Anshusharma42019@gmail.com>
2026-05-23 15:28:01 +0530

    Pai

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL = 'https://ecommerce-backend-xi-ten.vercel.app/api/v1';
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://ecommerce-backend-xi-ten.vercel.app/api/v1';
+// const API_BASE_URL = 'http://localhost:5000/api/v1';
 
 // Image URL formatter helper for local uploads static serving
 function formatImageUrl(url) {
